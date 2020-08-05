bf648918819ba7d2f4c982a53708218b5a93ef1f
nicol_go <gongqifeng.vip@gmail.com>
2020-08-05 16:18:56 +0800

    learn how to make a socket

---
+++ network_programming/make_socket.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int make_socket(uint16_t port)
+{
+    int sock;
+    struct sockaddr_in name;
+    /* create IPV4 socket. */
+    sock = socket(PF_INET,SOCK_STREAM,0);
+    if(sock < 0){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    /* bind to port and ip. */
+    name.sin_family = AF_INET;
+    name.sin_port = htons(port);
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if(bind(sock,(struct sockaddr *)&name, sizeof(name)) < 0){
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    return sock;
+}
+
+int main(int argc, char **argv){
+    int sockfd = make_socket(12345);
+    exit(0);
+}
